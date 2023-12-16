24c6a2a7a8d684e8b16c0f1f2bcc6722383228ca
Vitor <vitordacruz@gmail.com>
2023-12-16 15:19:36 -0300

    Ajuste ao Cadastrar Usuário

--- src/app/components/add-usuario/add-usuario.component.ts
+++ src/app/components/add-usuario/add-usuario.component.ts
@@ -12,6 +12,8 @@ export class AddUsuarioComponent {
   usuario = new Usuario();
   passwordConfirm = "";
   showModal = false;
+  carro = new Carro();
+  anoCarro:any = "";
 
   private readonly formatoData = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
 
@@ -31,17 +33,16 @@ export class AddUsuarioComponent {
   saveUsuario(): void {
     if (this.validUsuario()) {
       console.log('this.usuario', this.usuario);
+      this.usuario.cars = [];
+      this.usuario.cars.push(this.carro);
+
       this.usuarioService.create(this.usuario).subscribe({
         next: (res) => {
           console.log(res);
           this.submitted = true;
         },
         error: (e) => {
-          if (e && e.error && e.error.message) {
-            this.messagesErrors.push(e.error.message);
-          }
-          this.openModel();
-          console.error(e);
+          this.alertErro(e);
         }
       });
     }
@@ -50,6 +51,9 @@ export class AddUsuarioComponent {
   newUsuario(): void {
     this.submitted = false;
     this.usuario = new Usuario();
+    this.carro = new Carro();
+    this.anoCarro = "";
+    this.passwordConfirm = "";
   }
 
   validUsuario(): boolean {
@@ -96,6 +100,38 @@ export class AddUsuarioComponent {
         valido = false;
         mensagem.push("A Senha e a Confirmação da Senha não coincidem \n");
       }
+      if (this.carro) {
+        if (!this.carro.color || this.carro.color.trim() === "") {
+          valido = false;
+          mensagem.push("Preencha a cor do Carro \n");
+        }
+        if (!this.carro.licensePlate || this.carro.licensePlate.trim() === "") {
+          valido = false;
+          mensagem.push("Preencha a placa do Carro \n");
+        }
+        if (!this.carro.model || this.carro.model.trim() === "") {
+          valido = false;
+          mensagem.push("Preencha o modelo do Carro \n");
+        }
+        if (!this.anoCarro || this.anoCarro.trim() === '') {
+          valido = false;
+          mensagem.push("Preencha o ano do Carro \n");
+        } else {
+          if (isNaN(this.anoCarro)) {
+            valido = false;
+            mensagem.push("O ano do Carro deve ver um número \n");
+          } else {
+            try {
+              console.log('this.anoCarro', this.anoCarro);
+              const ano = parseInt(this.anoCarro);
+              this.carro.year = ano;
+            } catch (error) {
+              valido = false;
+              mensagem.push("O ano do Carro deve ver um número \n");
+            }
+          }
+        }
+      }
       if (!valido) {
         this.messagesErrors = mensagem;
         this.openModel();
@@ -141,6 +177,25 @@ export class AddUsuarioComponent {
       this.messagesErrors = [];
       this.showModal = false;
     }
+  }
+
+  alertErro(e: any) {
+    console.error(e);
+    this.messagesErrors = [];
+    if (e && e.error) {
+      console.log("e.error", e.error);
+      if (e.error.message) {
+        this.messagesErrors.push(e.error.message);
+      } else if(e.message) {
+        this.messagesErrors.push(e.message);
+      } else {
+        console.log("Erro message vazio");
+      }
+    } else if(e.message) {
+      console.log("e.message", e.message);
+      this.messagesErrors.push(e.message);
+    }
+    this.openModel();
   }
 
 }
